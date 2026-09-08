bca7e249eb6a7cb188966006e1c17d6b56faf946
DiegoLop87 <d.lopez@eston.ecotec.edu.ec>
2026-09-07 19:45:40 -0500

    definir la clase modelo Book con atributos inmutables

---
+++ lib/models/book.dart
@@ -0,0 +1,22 @@
+class Book {
+  final String id;
+  final String titulo;
+  final String autor;
+  final String categoria;
+  final String descripcion;
+  final double rating;
+  final String imagenUrl;
+  final int paginas;
+
+  Book({
+    required this.id,
+    required this.titulo,
+    required this.autor,
+    required this.categoria,
+    required this.descripcion,
+    required this.rating,
+    required this.imagenUrl,
+    required this.paginas,
+  });
+
+}
